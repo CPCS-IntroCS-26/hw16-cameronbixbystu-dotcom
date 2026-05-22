2e4d80e979a4e43d360855d7b288029c8f1db421
cameronbixbystu-dotcom <cameron.bixby.stu@cedarpark.org>
2026-05-22 09:47:36 -0700

    sub

--- animals.py
+++ animals.py
@@ -16,26 +16,94 @@ class Animal:
 
 
 class Dog(Animal):
-    def __init__(self, name, age, breed):
-        pass
+    def __init__(self, name, age, breed, sound):
+        self.__name = name
+        self.__age = age
+        self.__breed = breed
+        self.__sound = sound
 
 
+    def set_name(self, name):
+        self.__name = name
+
+
+    def set_age(self, age):
+        self.__age = age
+
+
+    def set_breed(self, breed):
+        self.__breed = breed
+
+
+    def set_sound(self, sound):
+        self.__sound = sound
+
+
+    def get_name(self):
+        return self.__name
+   
+    def get_age(self):
+        return self.__age
+   
+    def get_breed(self):
+        return self.__breed
+   
+    def __str__(self):
+        return f'Name: {self.__name}\n' + \
+            f'Age: {self.__age}\n' + \
+            f'Breed: {self.__breed}'
 
     def speak(self):
         Animal.speak = f"{name} says Woof."
         pass
 
     def move(self):
-        Animal.move = f"{name} runs 200 mi."
+        Animal.move = f"{name} runs on four legs."
         pass
 
 
 class Bird(Animal):
-    def __init__(self, name, age, can_fly):
+    def __init__(self, name, age, breed):
+        self.__name = name
+        self.__age = age
+        self.__can_fly = can_fly
+
+
+    def set_name(self, name):
+        self.__name = name
+
+
+    def set_type(self, age):
+        self.__age = age
+
+
+    def set_age(self, can_fly):
+        self.__can_fly = can_fly
+
+
+    def get_name(self):
+        return self.__name
+   
+    def get_type(self):
+        return self.__age
+   
+    def get_age(self):
+        return self.__can_fly
+   
+    def __str__(self):
+        return f'Name: {self.__name}\n' + \
+            f'Age: {self.__age}\n' + \
+            f'Breed: {self.__can_fly}'
+
+    def speak(self):
+        Animal.speak = f"{name} says caw caw."
         pass
 
     def move(self):
+        Animal.move = f"{name} flies through the air."
         pass
+
+    
 
 
 class Fish(Animal):
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from animals import Animal, Dog, Bird, Fish, Cat
 
+def dog():
+    name = input("What is the dog's name. ")
+    age = input("What is the dog's age. ")
+    breed = input("What breed is the dog. ")
+    dog = Dog(name, age, breed)
+    return dog
+
 
 def main():
     # Create one instance of each animal subclass
-    animals = []
     
 
+    dog_input = dog()
+
+
+    animals = [dog_input]
+    
+    print(animals)
     # TODO: instantiate your animals and add them to the list
 
     # Loop over all animals and call speak(), move(), and describe()
@@ -14,194 +26,4 @@ def main():
         
 
 if __name__ == "__main__":
-    main()
-
-from animals import Animal, Dog, Bird, Fish, Cat
-import pickle
-
-
-
-
-LOOK_UP = 1
-ADD = 2
-CHANGE = 3
-DELETE = 4
-QUIT = 5
-
-
-FILENAME = 'pets.dat'
-
-
-def main():
-    mypets = load_pets()
-
-
-    choice = 0
-
-
-    while choice != QUIT:
-        choice = get_menu_choice()
-
-
-        if choice == LOOK_UP:
-            look_up(mypets)
-        elif choice == ADD:
-            add(mypets)
-        elif choice == CHANGE:
-            change(mypets)
-        elif choice == DELETE:
-            delete(mypets)
-
-
-    save_pets(mypets)
-
-
-
-
-
-
-def load_pets():
-    try:
-        input_file = open(FILENAME, 'rb')
-
-
-        pet_dct = pickle.load(input_file)
-
-
-        input_file.close()
-    except IOError:
-        pet_dct = {}
-
-
-    return pet_dct
-
-
-
-
-
-
-def get_menu_choice():
-    print()
-    print("Menu")
-    print("---------------------------")
-    print('1. Look up a car')
-    print('2. Add a new car')
-    print('3. Change an existing car')
-    print('4. Delete a car')
-    print("5. Quite the program")
-    print()
-
-
-    choice = int(input('Enter a valid choice: '))
-
-
-    return choice
-
-
-
-
-
-
-
-
-def look_up(mypets):
-    make = input("Enter a make: ")
-    print(mypets.get(make, 'That make is not found.'))
-
-
-
-
-
-
-
-
-def add(mypets):
-    make = input('Make: ')
-    year_model = input('year model: ')
-    speed = 0
-
-
-    entry = animal.Animal(make, year_model, speed)
-
-
-    if make not in mypets:
-        mypets[make] = entry
-        print("The entry has been added.")
-    else:
-        print('That make already exists.')
-
-
-
-
-
-
-'''
-
-def change(mypets):
-    make = input('Enter a make: ')
-
-    if make in mypets:
-        year_model = input('Enter the new year_model: ')
-        speed = input('Enter the new speed: ')
-        entry = car.Car(make, year_model, speed)
-        mypets[make] = entry
-        print('Information updated.')
-    else:
-        print('That make is not found.')
-'''
-
-
-
-
-def change(mypets):
-    make = input('Enter a make: ')
-
-    if make in mypets:
-        speed = mypets[speed] + 5
-        entry = car.Car(make, speed)
-        mypets[make] = entry
-        print('Information updated.')
-    else:
-        print('That make is not found.')
-
-
-
-
-
-def delete(mypets):
-    make = input('Enter a make: ')
-    if make in mypets:
-        del mypets[make]
-        print('Entry deleted.')
-    else:
-        print('That make is not found.')
-
-
-
-
-
-
-
-
-def save_pets(mypets):
-    output_file = open(FILENAME, 'wb')
-
-
-    pickle.dump(mypets, output_file)
-
-
-    output_file.close()
-
-
-
-def acceleration(mypets):
-    make = input('Enter a make: ')
-    if make in mypets:
-        speed = make + 5
-
-
-
-
-
-if __name__ == '__main__':
     main()
